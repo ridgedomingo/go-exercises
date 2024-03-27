44a363436ccf267ce7f7cd930609b64b67fc6d10
ridge domingo <domingoridge@gmail.com>
2024-03-27 11:50:38 +0800

    Create function to generate pin

--- generator/generator.go
+++ generator/generator.go
@@ -1,7 +1,11 @@
 package generator
 
 import (
+	"crypto/rand"
 	"flag"
+	"fmt"
+	"log"
+	"math/big"
 )
 
 var(
@@ -14,18 +18,20 @@ var(
 
 func Start() {
 
-	flag.IntVar(&length, "length", 12, "password length")
+	flag.IntVar(&length, "length", 0, "password length")
 	flag.BoolVar(&isNumbersIncluded, "includeNumbers", false, "should include numbers")
 	flag.BoolVar(&isSymbolsIncluded, "includeSymbols", false, "should include symbols")
 	flag.BoolVar(&isUppercaseIncluded, "includeUppercase", false, "should include uppercase")
 	flag.StringVar(&passwordType, "type", "random", "password type")
 	flag.Parse()
 
-	generatePassword(length,isNumbersIncluded,isSymbolsIncluded,isUppercaseIncluded,passwordType)
+	password := generatePassword()
+	fmt.Println("Generated password", password)
 
 }
 
-func generatePassword(length int,isNumbersIncluded bool,isSymbolsIncluded bool,isUppercaseIncluded bool,passwordType string) {
+func generatePassword() string {
+	var generatedPassword string
 	switch passwordType {
     case "random":
 		  // TO DO: Func to create random password
@@ -33,6 +39,37 @@ func generatePassword(length int,isNumbersIncluded bool,isSymbolsIncluded bool,i
 		  // TO DO: Func to create alphanumeric password
     case "pin":
 		  // TO DO: Func to create pin
+		generatedPassword = generatePin()
     }
 
+	return generatedPassword
+}
+
+func generatePin() string {
+	// default pin to 6 digits
+	pinLength := 6
+
+	if length >= 1 {
+		pinLength = length
+	}
+	randomNum, err := rand.Int(rand.Reader, big.NewInt(int64(pow(10, pinLength)-pow(10, pinLength-1))))
+	if err != nil {
+		log.Fatal(err)
+	}
+	randomNum = randomNum.Add(randomNum, big.NewInt(int64(pow(10, pinLength-1))))
+
+
+	return randomNum.String()
+}
+
+// Function to calculate power
+func pow(x, y int) int {
+	if y == 0 {
+		return 1
+	}
+	result := x
+	for i := 1; i < y; i++ {
+		result *= x
+	}
+	return result
 }
